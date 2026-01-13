58c48ec3e916fede91995c375d63c49e9287c532
kkanyo <kkanyo33@gmail.com>
2026-01-13 19:07:31 +0900

    Dev environment setup

---
+++ backend/src/main/java/com/kkanyo/gf2tool/Gf2toolApplication.java
@@ -0,0 +1,13 @@
+package com.kkanyo.gf2tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gf2toolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gf2toolApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/kkanyo/gf2tool/Gf2toolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkanyo.gf2tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gf2toolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
